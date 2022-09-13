0edd8c074dac6158092ea4bbba22569056a9b577
Florin Neuro <109140967+FlorinNeuro@users.noreply.github.com>
2022-09-13 20:21:51 +0200

    Add files via upload

---
+++ Hilbert Envelope/HilbertEnvelope_smallband_fir.m
@@ -0,0 +1,34 @@
+function HilbertEnvelope_smallband_fir(Data,sRate,frequency_bins, run_subject, OutputPath)
+%Aim reproduce Brookes et al. 2011 PNAS
+%Determine envelope in frequency_bins from Data and resample to 1Hz
+
+%Update 8.11.2016: zscore is put back in for the filtered data, because otherwise
+% there would be no correction for the 1/f decrease
+% In addition the frequency band width was corrected. Before it read frequency_bins(ff,2)<30 && frequency_bins(ff,2)>4Brookes approach
+
+% Esther Florin 22.10.2016
+dl=length(Data);
+for ff=1:length(frequency_bins)
+    if frequency_bins(ff,2)<=30 && frequency_bins(ff,2)>4
+        freqbands=frequency_bins(ff,1):2:frequency_bins(ff,2);
+    elseif frequency_bins(ff,2)>30
+        freqbands=frequency_bins(ff,1):5:frequency_bins(ff,2);
+    elseif frequency_bins(ff,2)<=4
+        freqbands=frequency_bins(ff,1):1:frequency_bins(ff,2);
+        
+    end
+    temp=zeros(length(freqbands)-1,size(Data,1),size(Data,2));
+    for ff_2=1:length(freqbands)-1
+        
+        temp(ff_2,:,:)=bst_bandpass_filtfilt(Data, sRate,freqbands(ff_2),freqbands(ff_2+1),0,'fir'); %%iir filter ensures that low-pass has not a too large passband
+            temp(ff_2,:,:)= bsxfun(@rdivide,...
+            bsxfun(@minus, squeeze(temp(ff_2,:,:)),mean(squeeze(temp(ff_2,:,:)),2)),std(squeeze(temp(ff_2,:,:)),[],2));%/std(temp(ff_2,:,:));
+
+        temp(ff_2,:,:)=(abs(hilbert(squeeze(temp(ff_2,:,:))')))';
+    end
+    temp=squeeze(mean(temp,1));
+    [Data_f(ff,:,:),timeout]=process_resample('Compute',temp,(0:dl-1)/sRate,1,'resample-cascade');
+    clear temp
+end
+save([OutputPath 'HilbertEnvelope_small_band_fir' run_subject ],'Data_f' ,'timeout','frequency_bins','-v7.3')
+
---
+++ Hilbert Envelope/HilbertEnvelope_wideBand_fir.m
@@ -0,0 +1,19 @@
+function HilbertEnvelope_wideBand_fir(Data,sRate,frequency_bins, run_subject, OutputPath)
+%Aim reproduce Brookes et al. 2011 PNAS
+%Determine envelope across whole bands (delta, theta, alpha,...) without a
+%correction for 1/f and with the hilbert transform across the whole band!
+%This seems to be the way Brookes et al. performed the analysis
+%zscore is taken out of function. Makes it closer to
+%Brookes approach
+
+% Esther Florin 05.03.2018
+dl=length(Data);
+
+for ff=1:length(frequency_bins)
+        temp=bst_bandpass_filtfilt(Data, sRate,frequency_bins(ff,1),frequency_bins(ff,2),0,'fir'); %%iir filter ensures that low-pass has not a too large passband
+        temp=(abs(hilbert((temp)')))';
+    [Data_f(ff,:,:),timeout]=process_resample('Compute',temp,(0:dl-1)/sRate,1,'resample-cascade');
+    clear temp
+end
+save([OutputPath 'HilbertEnvelope_wideBand_fir' run_subject ],'Data_f' ,'timeout','frequency_bins','-v7.3')
+
